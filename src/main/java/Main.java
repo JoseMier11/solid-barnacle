180fe49ec99e5215739a15d6158f7aaab2e2d521
JoseMier11 <111892426+JoseMier11@users.noreply.github.com>
2022-08-27 09:26:51 -0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 public class Main {
     @RequestMapping("/")
     String home() {
-        return "Mi primer azure v3!";
+        return 
 <title> Dragon Ball Z <title>
     }
     
